63a615a792f01df5535dfa1497620f20a910ac4b
Chin <mcaiox@icloud.com>
2020-11-30 18:47:26 +0000

    A simple hashtable solution using both Dictionary and Hashtable

---
+++ FindMinimumInRotatedSortedArray/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace FindMinimumInRotatedSortedArray
+{
+    class FMIRSA
+    {
+        public static int findMin(int[] num)
+        {
+            if (num == null || num.Length == 0)
+            {
+                return Int32.MinValue;
+            }
+
+            int start = 0;
+            int end = num.Length - 1;
+            /*            Console.WriteLine("num[start] is " + num[start]);*/
+            while (start + 1 < end)
+            {
+
+                int mid = start + (end - start) / 2;
+                /*                Console.WriteLine("num[start] is " + num[start]);
+                                Console.WriteLine("num[mid] is " + num[mid]);
+                                Console.WriteLine("num[end] is " + num[end]);*/
+                if (num[start] < num[end])
+                {
+                    end = mid;
+                }
+                else if (num[start] < num[mid])
+                {
+                    start = mid;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+            /*            Console.WriteLine("num[start] is " + num[start]);
+                        Console.WriteLine("num[end] is " + num[end]);*/
+            if (num[start] < num[end])
+            {
+                return num[start];
+            }
+            else
+            {
+                return num[end];
+            }
+
+        }
+    }
+    class Program : FMIRSA
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, 5, 1, 2 };
+            Console.WriteLine(findMin(arr));
+        }
+
+
+        
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HashTable
+{
+    class Program
+    {
+        static Hashtable userInfoHash;
+        static void Main(string[] args)
+        {
+
+
+            //Console.WriteLine("Hello World!");
+            userInfoHash = new Hashtable();
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+
+
+            //Adding
+            for (int i = 0; i < 10; i++)
+            {
+                userInfoHash.Add(i, "user" + i);
+                myDictionary.Add(i, "user" + i);
+            }
+
+            //Removing
+            if (userInfoHash.ContainsKey(0))
+            {
+                userInfoHash.Remove(0);
+            }
+
+            //Setting 
+            if (userInfoHash.ContainsKey(1))
+            {
+                userInfoHash[1] = "Chin";
+            }
+
+            //Looping
+
+            foreach (DictionaryEntry entry in userInfoHash)
+            {
+                Console.WriteLine("Key: " + entry.Key + " => Value: " + entry.Value);
+            }
+
+            foreach (KeyValuePair<int, string> element in myDictionary)
+            {
+                Console.WriteLine("Key:- {0} and Value:- {1}",
+                                  element.Key, element.Value);
+            }
+
+
+        }
+    }
+}
